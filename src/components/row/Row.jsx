5894f61580526b6cc02863303ac92cdfae147768
CharlesCypher <99364836+CharlesCypher@users.noreply.github.com>
2023-08-16 09:50:08 +0100

    movie request

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -5,20 +5,29 @@ import Card from "../card/Card";
 
 const Row = ({ title, getMovies }) => {
   const [movies, setMovies] = useState([]);
-  const moviesQuery = useQuery({
-    queryKey: ["movie"],
-    queryFn: async () => {
-      const response = await axios.get(getMovies);
-      const data = await response.data;
-      return data;
-    },
-  });
   useEffect(() => {
-    setMovies(moviesQuery?.data?.results);
-  }, [moviesQuery.data]);
+    async function fetchData() {
+      const res = await axios.get(getMovies);
+      setMovies(res.data.results);
+    }
+    fetchData();
+  }, [getMovies]);
   console.log(movies);
-  if (moviesQuery.isLoading) return <h1>Loading....</h1>;
-  if (moviesQuery.isError) return <h1>Error loading data!!!</h1>;
+  // const moviesQuery = useQuery({
+  //   queryKey: ["movies"],
+  //   queryFn: async () => {
+  //     const response = await axios.get(getMovies);
+  //     const data = await response.data;
+  //     console.log(response);
+  //     return data;
+  //   },
+  // });
+  // useEffect(() => {
+  //   setMovies(moviesQuery?.data?.results);
+  // }, [moviesQuery.data]);
+  // if (moviesQuery.isLoading) return <h1>Loading....</h1>;
+  // if (moviesQuery.isError) return <h1>Error loading data!!!</h1>;
+
   return (
     <section className="py-8">
       <div className="px-6 -mb-4">
--- src/constants/requests.js
+++ src/constants/requests.js
@@ -1,9 +1,7 @@
 const API_KEY = import.meta.env.VITE_REACT_APP_TMDB_API_KEY;
 
-console.log(API_KEY);
-
 const requests = {
-  getTrending: `trending/movie/day?api_key=${API_KEY}&language=en-US`,
+  getTrendingMovies: `trending/movie/day?api_key=${API_KEY}&language=en-US`,
   getActionMovies: `discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=28`,
   getComedyMovies: `discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=35`,
   getHorrorMovies: `discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=27`,
